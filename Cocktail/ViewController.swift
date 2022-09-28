e83af58bdd44275c650975be4387b1f4384b0a97
Ali Demirtaş <alidemirtas.7@icloud.com>
2022-09-28 23:29:24 +0300

    Initial Commit

---
+++ Cocktail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktail
+//
+//  Created by Ali Demirtaş on 28.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
